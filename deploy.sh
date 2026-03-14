9c16a48b5c70dab841149fb9bf183ab7315f821b
Ubuntu <ubuntu@ip-172-31-68-253.ec2.internal>
2026-03-14 10:07:45 +0000

    testing pipeline

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@
 docker login -u adikhare31 -p PurvaManu_0302 
 
 
-if [ "$GIT_BRANCH" = "dev" ]; then 
-	docker tag nginx_image aditikhare31/reactjs-dev 
-	docker push aditikhare31/reactjs-dev:latest 
-elif [ "$GIT_BRANCH" = "main" ]; then 
-	docker tag nginx_image aditikhare31/reactjs-prod 
-	docker push aditikhare31/reactjs-prod:latest 
+if [ "$BRANCH_NAME" = "dev" ]; then 
+	docker tag nginx_image adikhare31/reactjs-dev 
+	docker push adikhare31/reactjs-dev:latest 
+elif [ "$BRANCH_NAME" = "main" ]; then 
+	docker tag nginx_image adikhare31/reactjs-prod 
+	docker push adikhare31/reactjs-prod:latest 
 else 
 	echo "No branch" 
 fi
