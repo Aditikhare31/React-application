535261c6971e3781a82113bb4c94243fc60c135d
Ubuntu <ubuntu@ip-172-31-68-253.ec2.internal>
2026-03-14 08:41:57 +0000

    fixed authentication error

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [ "$GIT_BRANCH" = "origin/dev" ]; then
 	docker tag nginx_image aditikhare31/reactjs-dev 
 	docker push aditikhare31/reactjs-dev:latest 
 elif [ "$GIT_BRANCH" = "origin/main" ]; then 
-	docker tag image aditikhare31/reactjs-prod 
+	docker tag nginx_image aditikhare31/reactjs-prod 
 	docker push aditikhare31/reactjs-prod:latest 
 else 
 	echo "No branch" 
