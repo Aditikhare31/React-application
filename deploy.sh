a2c0aed2ac7fa0399eed554e80ab5ac1b608e13f
Ubuntu <ubuntu@ip-172-31-68-253.ec2.internal>
2026-03-14 08:36:39 +0000

    fixed authentication error

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 docker login -u adikhare31 -p PurvaManu_0302 
 
-if [ "$GIT_BRANCH" = "dev" ]; then 
+if [ "$GIT_BRANCH" = "origin/dev" ]; then 
 	docker tag nginx_image aditikhare31/reactjs-dev 
 	docker push aditikhare31/reactjs-dev:latest 
-elif [ "$GIT_BRANCH" = "main" ]; then 
+elif [ "$GIT_BRANCH" = "origin/main" ]; then 
 	docker tag image aditikhare31/reactjs-prod 
 	docker push aditikhare31/reactjs-prod:latest 
 else 
