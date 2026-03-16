c92c312d79aface7d696b7e06e820eb23c90da6c
Ubuntu <ubuntu@ip-172-31-68-253.ec2.internal>
2026-03-16 08:30:17 +0000

    Added file

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,14 @@
 
 docker login -u adikhare31 -p PurvaManu_0302
 
-if [ "$GIT_BRANCH" = "origin/dev"]; then
+if [ "$GIT_BRANCH" = "origin/dev" ]; then
 	docker tag nginx_image adikhare31/reactjs-dev
 	docker push adikhare31/reactjs-dev:latest
-elif [ "$GIT_BRANCH" = "origin/main"]; then
+elif [ "$GIT_BRANCH" = "origin/main" ]; then
 	docker tag nginx_image adikhare31/reactjs-prod
         docker push adikhare31/reactjs-prod:latest
 else
 	echo "No branch matched"
 fi
+
+docker compose up -d
