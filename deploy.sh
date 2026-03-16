2c467e099a3a7a58ce92f79662fff080271d587d
Ubuntu <ubuntu@ip-172-31-68-253.ec2.internal>
2026-03-16 07:11:17 +0000

    error correction

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 docker login -u adikhare31 -p PurvaManu_0302 
 
 
-if [ "$GIT_BRANCH" = "dev" ]; then 
+if [ "$GIT_BRANCH" = "origin/dev" ]; then 
 	docker tag nginx_image adikhare31/reactjs-dev 
 	docker push adikhare31/reactjs-dev:latest 
-elif [ "$GIT_BRANCH" = "main" ]; then 
+elif [ "$GIT_BRANCH" = "origin/main" ]; then 
 	docker tag nginx_image adikhare31/reactjs-prod 
 	docker push adikhare31/reactjs-prod:latest 
 else 
